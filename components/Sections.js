f19e8f884d85edc20c513f979947be138c45cfbc
cryptofool13 <fender18295@gmail.com>
2021-03-18 07:53:56 -0400

    working version of section tab-able. needs animation next

--- components/Sections.js
+++ components/Sections.js
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
+import styles from "../styles/Sections.module.css";
+
 export function Sections() {
     const [selected, setSelected] = useState(0);
 
@@ -10,7 +12,7 @@ export function Sections() {
         sections[selected].current.style = "height: 60vh";
         sections.forEach((sec, i) => {
             if (i !== selected) {
-                sec.current.style = "height: unset";
+                sec.current.style = "height: 80px";
             }
         });
     }, [selected]);
@@ -20,30 +22,66 @@ export function Sections() {
             <section
                 onClick={() => setSelected(0)}
                 ref={sec1}
-                className="closed"
+                className={styles.first}
             >
-                Work Experience
+                <h2
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            setSelected(0);
+                        }
+                    }}
+                    tabIndex="1"
+                >
+                    Work Experience
+                </h2>
             </section>
             <section
                 onClick={() => setSelected(1)}
                 ref={sec2}
-                className="closed"
+                className={styles.second}
             >
-                Code Samples
+                <h2
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            setSelected(1);
+                        }
+                    }}
+                    tabIndex="2"
+                >
+                    Code Samples
+                </h2>
             </section>
             <section
                 onClick={() => setSelected(2)}
                 ref={sec3}
-                className="closed"
+                className={styles.third}
             >
-                Cat Pictures
+                <h2
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            setSelected(2);
+                        }
+                    }}
+                    tabIndex="3"
+                >
+                    Cat Pictures
+                </h2>
             </section>
             <section
                 onClick={() => setSelected(3)}
                 ref={sec4}
-                className="closed"
+                className={styles.fourth}
             >
-                Blog Posts
+                <h2
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            setSelected(3);
+                        }
+                    }}
+                    tabIndex="4"
+                >
+                    Blog Posts
+                </h2>
             </section>
         </>
     );
