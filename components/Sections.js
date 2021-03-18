156a0ba7d2acb244fc9136d3e85e679186c7c356
cryptofool13 <fender18295@gmail.com>
2021-03-18 12:58:21 -0400

    expandable sections with react-spring

--- components/Sections.js
+++ components/Sections.js
@@ -1,88 +1,93 @@
 import { useEffect, useRef, useState } from "react";
+import { useSpring, animated } from "react-spring";
 
 import styles from "../styles/Sections.module.css";
 
 export function Sections() {
     const [selected, setSelected] = useState(0);
 
-    const sections = [useRef(), useRef(), useRef(), useRef()];
-    const [sec1, sec2, sec3, sec4] = sections;
-
-    useEffect(() => {
-        sections[selected].current.style = "height: 60vh";
-        sections.forEach((sec, i) => {
-            if (i !== selected) {
-                sec.current.style = "height: 80px";
-            }
-        });
-    }, [selected]);
-
     return (
         <>
-            <section
-                onClick={() => setSelected(0)}
-                ref={sec1}
-                className={styles.first}
-            >
-                <h2
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            setSelected(0);
-                        }
-                    }}
-                    tabIndex="1"
+            <ExpandableSection selected={selected === 0}>
+                <section
+                    className={styles.first}
+                    onClick={() => setSelected(0)}
                 >
-                    Work Experience
-                </h2>
-            </section>
-            <section
-                onClick={() => setSelected(1)}
-                ref={sec2}
-                className={styles.second}
-            >
-                <h2
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            setSelected(1);
-                        }
-                    }}
-                    tabIndex="2"
+                    <h2
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                setSelected(0);
+                            }
+                        }}
+                        tabIndex="1"
+                    >
+                        Work Experience
+                    </h2>
+                </section>
+            </ExpandableSection>
+            <ExpandableSection selected={selected === 1}>
+                <section
+                    onClick={() => setSelected(1)}
+                    className={styles.second}
                 >
-                    Code Samples
-                </h2>
-            </section>
-            <section
-                onClick={() => setSelected(2)}
-                ref={sec3}
-                className={styles.third}
-            >
-                <h2
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            setSelected(2);
-                        }
-                    }}
-                    tabIndex="3"
+                    <h2
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                setSelected(1);
+                            }
+                        }}
+                        tabIndex="2"
+                    >
+                        Code Samples
+                    </h2>
+                </section>
+            </ExpandableSection>
+            <ExpandableSection selected={selected === 2}>
+                <section
+                    onClick={() => setSelected(2)}
+                    className={styles.third}
                 >
-                    Cat Pictures
-                </h2>
-            </section>
-            <section
-                onClick={() => setSelected(3)}
-                ref={sec4}
-                className={styles.fourth}
-            >
-                <h2
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            setSelected(3);
-                        }
-                    }}
-                    tabIndex="4"
+                    <h2
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                setSelected(2);
+                            }
+                        }}
+                        tabIndex="3"
+                    >
+                        Cat Pictures
+                    </h2>
+                </section>
+            </ExpandableSection>
+            <ExpandableSection selected={selected === 3}>
+                <section
+                    onClick={() => setSelected(3)}
+                    className={styles.fourth}
                 >
-                    Blog Posts
-                </h2>
-            </section>
+                    <h2
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                setSelected(3);
+                            }
+                        }}
+                        tabIndex="4"
+                    >
+                        Blog Posts
+                    </h2>
+                </section>
+            </ExpandableSection>
         </>
     );
+}
+
+function ExpandableSection({ selected, children }) {
+    const [vh, setVh] = useState(0);
+    useEffect(() => {
+        setVh(window.innerHeight);
+    });
+    const props = useSpring({
+        from: { height: "80px" },
+        to: { height: selected ? `${vh * 0.6}px` : "80px" },
+    });
+    return <animated.div style={props}>{children}</animated.div>;
 }
