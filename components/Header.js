37e735167eac8ffededcc3f52f11562f024ec29e
michael curry <mikedubcurry@pop-os.localdomain>
2021-03-22 16:21:24 -0400

    useReducedMot and has mounted

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../styles/Header.module.css';
 
 const descs = [
@@ -10,6 +10,9 @@ const descs = [
 
 export default function Header() {
 	const [desc, setDesc] = useState(descs[0]);
+	const hasMounted = useHasMounted();
+	const reducedMotion = usePrefersReducedMotion();
+	console.log(reducedMotion);
 	return (
 		<header className={styles.header}>
 			<div>
@@ -17,4 +20,34 @@ export default function Header() {
 			</div>
 		</header>
 	);
+}
+
+function useHasMounted() {
+	const [hasMounted, setHasMounted] = useState(false);
+	useEffect(() => {
+		setHasMounted(true);
+	}, []);
+	return hasMounted;
+}
+
+const QUERY = '(prefers-reduced-motion: no-preference)';
+const isRenderingOnServer = typeof window === 'undefined';
+const getInitialState = () => {
+	return isRenderingOnServer ? true : !window.matchMedia(QUERY).matches;
+};
+function usePrefersReducedMotion(DEV) {
+	const [prefersReducedMotion, setPrefersReducedMotion] = useState(
+		!DEV ? getInitialState : true
+	);
+	useEffect(() => {
+		const mediaQueryList = window.matchMedia(QUERY);
+		const listener = (event) => {
+			setPrefersReducedMotion(!event.matches);
+		};
+		mediaQueryList.addListener(listener);
+		return () => {
+			mediaQueryList.removeListener(listener);
+		};
+	}, []);
+	return prefersReducedMotion;
 }
