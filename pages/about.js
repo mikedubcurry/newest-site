64326722ca19b472fb7759abe34d4af8d69b764c
Michael Curry <michaelcurry95@gmail.com>
2021-04-28 19:24:16 -0400

    smooth scroll if not prefersreducedmotion

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,48 @@
 import styles from '../styles/About.module.css';
+import {useHasMounted, usePrefersReducedMotion} from '../hooks'
 
 export default function About() {
+	const prefersReducedMotion = usePrefersReducedMotion();
+	const hasMounted = useHasMounted();
+
+
+	
+	const handleNavLinkClick = (e) => {
+		console.log(prefersReducedMotion, hasMounted);
+		if(prefersReducedMotion) {
+			return
+		}
+		e.preventDefault();
+		const id = e.target.getAttribute('href');
+		const section = document.querySelector(id);
+		section.scrollIntoView({ behavior: 'smooth' });
+		setTimeout(() => {
+			section.focus()
+		}, 300)
+	};
 	return (
 		<main className={styles.main}>
 			<h1 className={styles['page-title']}>About</h1>
 			<nav>
 				<ul>
 					<li>
-						<a href="#this-site">This Site</a>
+						<a href="#this-site" onClick={handleNavLinkClick}>This Site</a>
 					</li>
 					<li>
-						<a href="#projects">My Other Projects</a>
+						<a href="#projects" onClick={handleNavLinkClick}>My Other Projects</a>
 					</li>
 					<li>
-						<a href="#work">Work Experience</a>
+						<a href="#work" onClick={handleNavLinkClick}>Work Experience</a>
 					</li>
 					<li>
-						<a href="#hobbies">Hobbies</a>
+						<a href="#hobbies" onClick={handleNavLinkClick}>
+							Hobbies
+						</a>
 					</li>
 				</ul>
 			</nav>
-			<section>
-				<h2 id="this-site" className={styles.subtitle}>
+			<section className={styles["aboutpage-section"]}>
+				<h2 tabIndex={-1} id="this-site" className={styles.subtitle}>
 					This Site
 				</h2>
 				<p className={styles.copy}>
@@ -33,8 +54,8 @@ export default function About() {
 					about the process and technologies I used to build this site!
 				</p>
 			</section>
-			<section>
-				<h2 id="projects" className={styles.subtitle}>
+			<section className={styles["aboutpage-section"]}>
+				<h2 tabIndex={-1} id="projects" className={styles.subtitle}>
 					Other Projects
 				</h2>
 				<p className={styles.copy}>
@@ -42,27 +63,53 @@ export default function About() {
 					laudantium quis perferendis, explicabo odit suscipit eius aliquid
 					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
 					voluptatum in accusantium deserunt. Pariatur, deleniti!
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+					laudantium quis perferendis, explicabo odit suscipit eius aliquid
+					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
+					voluptatum in accusantium deserunt. Pariatur, deleniti!
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+					laudantium quis perferendis, explicabo odit suscipit eius aliquid
+					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
+					voluptatum in accusantium deserunt. Pariatur, deleniti!
 				</p>
 			</section>
-			<section>
-				<h2 id="work" className={styles.subtitle}>
+			<section className={styles["aboutpage-section"]}>
+				<h2 tabIndex={-1} id="work" className={styles.subtitle}>
 					Work Experience
 				</h2>
-				<p className={styles.copy}>					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+				<p className={styles.copy}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
 					laudantium quis perferendis, explicabo odit suscipit eius aliquid
 					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
 					voluptatum in accusantium deserunt. Pariatur, deleniti!
-</p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+					laudantium quis perferendis, explicabo odit suscipit eius aliquid
+					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
+					voluptatum in accusantium deserunt. Pariatur, deleniti!
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+					laudantium quis perferendis, explicabo odit suscipit eius aliquid
+					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
+					voluptatum in accusantium deserunt. Pariatur, deleniti!
+				</p>
 			</section>
-			<section>
-				<h2 className={styles.subtitle} id="hobbies">
+			<section className={styles["aboutpage-section"]}>
+				<h2 tabIndex={-1} className={styles.subtitle} id="hobbies">
 					Hobbies
 				</h2>
-				<p className={styles.copy}>					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+				<p className={styles.copy}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
 					laudantium quis perferendis, explicabo odit suscipit eius aliquid
 					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
 					voluptatum in accusantium deserunt. Pariatur, deleniti!
-</p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+					laudantium quis perferendis, explicabo odit suscipit eius aliquid
+					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
+					voluptatum in accusantium deserunt. Pariatur, deleniti!
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+					laudantium quis perferendis, explicabo odit suscipit eius aliquid
+					labore consequatur quibusdam dignissimos repudiandae fugit veritatis
+					voluptatum in accusantium deserunt. Pariatur, deleniti!
+				</p>
 			</section>
 		</main>
 	);
