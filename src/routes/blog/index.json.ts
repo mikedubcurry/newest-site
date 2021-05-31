edeef6b00f34eadd2e1a4e895e1b323fdf1e9f09
Michael Curry <michaelcurry95@gmail.com>
2021-05-31 10:15:28 -0400

    style blog post link

--- src/routes/blog/index.json.ts
+++ src/routes/blog/index.json.ts
@@ -6,20 +6,18 @@ import gray from 'gray-matter';
 const mdReader = md({ html: true });
 
 export async function get() {
-	// the `slug` parameter is available because this file
-	// is called [slug].json.js
 	const files = readdirSync(resolve('./src/posts'), { encoding: 'utf8' });
-	const posts = files.map((file) => {
+	const postData = files.map((file) => {
 		const post = readFileSync(resolve('./src/posts/' + file), { encoding: 'utf8' });
 
 		const gr = gray(post);
 		console.log(gr);
 		
-		return {data: gr.data};
+		return {...gr.data};
 	});
 
 	return {
 		status: 200,
-		body: posts
+		body: postData
 	};
 }
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -2,25 +2,57 @@
 	export async function load({ page, fetch }) {
 		const res = await fetch('/blog.json');
 		if (res.ok) {
-			let data = await res.json();
-      console.log(data);
-      
+			let posts = await res.json();
 			return {
-				status: 200,
-				body: data
-			};
-		} else {
-			console.log('res bad', res);
-			return {
-				status: 500,
-				body: 'hi'
+				props: {
+					posts
+				}
 			};
 		}
 	}
 </script>
 
 <script lang="ts">
-	console.log('props', $$props);	
+	export let posts: { title: string; slug: string; summary: string; tags: string[] }[];
 </script>
 
 <h1>Blog Posts</h1>
+
+{#each posts as post}
+	<article>
+		<h2><a href={`/blog/${post.slug}`}>{post.title}</a></h2>
+		{#each post.tags as tag}
+			<span>{tag}</span>&nbsp;
+		{/each}
+		<p>{post.summary}</p>
+	</article>
+{/each}
+
+<style>
+	article {
+		max-width: 800px;
+		transition: background-color .3s ease; 
+		padding: 2rem;
+		border-radius: 14px;
+	}
+	h2 {
+		margin-block-start: 0;
+	}
+	a {
+		font-size: var(--medium-font-size)
+	}
+
+	span {
+		background-color: var(--secondary-color);
+		border-radius: 14px;
+		transition: background-color .3s ease; 
+		padding: 0.5rem;
+	}
+
+	article:hover {
+		background-color: var(--secondary-color);
+	}
+	article:hover span {
+		background-color: var(--primary-color);
+	}
+</style>
