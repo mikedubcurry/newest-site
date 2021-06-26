c764d68da686a5ae4ed02c0724800154d4afd767
Michael Curry <michaelcurry95@gmail.com>
2021-06-26 16:35:37 -0400

    fix blog path

--- src/routes/blog/index.json.ts
+++ src/routes/blog/index.json.ts
@@ -2,15 +2,14 @@ import { readFileSync, readdirSync } from 'fs';
 import { resolve } from 'path';
 import gray from 'gray-matter';
 
-
 export async function get() {
-	const files = readdirSync(resolve('./src/posts'), { encoding: 'utf8' });
+	const files = readdirSync(resolve('src/_posts'), { encoding: 'utf8' });
 	const postData = files.map((file) => {
-		const post = readFileSync(resolve('./src/posts/' + file), { encoding: 'utf8' });
+		const post = readFileSync(resolve('src/_posts/' + file), { encoding: 'utf8' });
 
 		const gr = gray(post);
-		
-		return {...gr.data};
+
+		return { ...gr.data };
 	});
 
 	return {
