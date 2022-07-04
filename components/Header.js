87cd8025a7c9769846a9926679a52511648dc0b7
mikedubcurry <michaelcurry95@gmail.com>
2022-07-04 10:37:41 -0400

    some layout tweaks

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export const Header = () => {
 	return (
-		<header className="bg-gradient-to-b from-gray-400 to-transparent h-24 flex justify-center items-center">
+		<header className="fixed w-full z-20 bg-gradient-to-b from-gray-400 to-transparent h-24 flex justify-center items-center">
 			<span className="text-3xl font-mono">Mike Curry</span>
 		</header>
 	);
--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,10 @@ import { Header } from './Header';
 
 export const Layout = ({ children }) => {
 	return (
-		<div className='bg-primary min-h-screen'>
+		<div className='bg-primary min-h-screen h-full'>
 			<Header />
-			<main className='flex flex-col items-start px-2 prose prose-stone'>{children}</main>
+			<main className='flex flex-col relative z-10 pt-24 items-start px-2 prose prose-stone'>{children}</main>
+			<footer className=''>Footer Area</footer>
 		</div>
 	);
 };
